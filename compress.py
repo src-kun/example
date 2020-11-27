bf9ac502d38c228601ba2ce0061f0d3fd9972ce5
src-kun <9331031@qq.com>
2020-11-27 23:59:37 +0800

    Update compress.py

--- compress.py
+++ compress.py
@@ -1,15 +1,24 @@
-#!/usr/bin/python3  
-# -*- coding: utf-8 -*-  
-
 import tarfile
-import bz2
 
-def tarbz2(source, target=None):
+
+def compression(source, target=None):
     if target is None:
         target = '%s.tar.bz2' % source
     archive = tarfile.open(target, 'w:bz2')
     # archive.debug = 1
-    # arcname specifies an alternative name for the file in the archive.
-    archive.add(source, source.split('/')[-1]) 
+
+    archive.add(source, source.split('\\')[-1])
     archive.close()
     return target
+
+
+def decompression(source, target):
+    tar = tarfile.open(source, "r:bz2")
+    file_names = tar.getnames()
+    for file_name in file_names:
+        tar.extract(file_name, target)
+    tar.close()
+
+
+print(compression('E:\\PycharmProjects\\test'))  # compression dir save to E:\PycharmProjects\test.tar.bz2
+decompression('E:\\PycharmProjects\\test.tar.bz2', target='e:\\')  # decompression dir to e:\\test
